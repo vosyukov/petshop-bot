5fc7f7477dee9ea9d4475bfd87d0b00576668da5
osyukov.v <osyukov.v@ideadlt.com>
2023-04-30 19:43:03 +0300

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+
+	r.GET("/:name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello, %s!", name),
+		})
+	})
+
+	r.Run(fmt.Sprintf(":%s", port))
+}
---
+++ test.go
@@ -0,0 +1,6 @@
+package main
+
+func SumInt(a int, b int) (sum int, err error) {
+	var w = a + b
+	return w, nil
+}
